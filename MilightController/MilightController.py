544fc7b782da5a79e074a8d60b3c8f852236b35a
wyz3r0 <dobroszek.d@gmail.com>
2024-05-28 10:55:14 +0200

    added typeing

--- MilightController/MilightController.py
+++ MilightController/MilightController.py
@@ -12,25 +12,25 @@ class MilightController:
     __PORT_v6: int = 5987
 
     def __init__(self, options: dict[str, str]) -> None:
-        self.options = options or {"type": "all"}
-        self.port = self.options.get("port", 48899)
-        self.host = self.options.get("address", "255.255.255.255")
-        self.timeout = self.options.get("timeout", 3000)
-        self.discover_legacy = (
+        self.options: dict[str, str] = options or {"type": "all"}
+        self.port: int = self.options.get("port", 48899)
+        self.host: str = self.options.get("address", "255.255.255.255")
+        self.timeout: int = self.options.get("timeout", 3000)
+        self.discover_legacy: str = (
             not self.options.get("type")
             or self.options.get("type") == "all"
             or self.options.get("type") == "legacy"
         )
-        self.discover_v6 = (
+        self.discover_v6: str = (
             self.options.get("type") == "all" or self.options.get("type") == "v6"
         )
-        self.discovery_message_legacy = b"\x4C\x69\x6E\x6B\x5F\x57\x69\x2D\x46\x69"
-        self.discovery_message_v6 = (
+        self.discovery_message_legacy: bytes = b"\x4C\x69\x6E\x6B\x5F\x57\x69\x2D\x46\x69"
+        self.discovery_message_v6: bytes = (
             b"\x48\x46\x2D\x41\x31\x31\x41\x53\x53\x49\x53\x54\x48\x52\x45\x41\x44"
         )
-        self.timeout_id = None
-        self.disco_results = []
-        self.sequenceNumber = 0
+        # self.timeout_id = None
+        self.disco_results: list[dict[str, str]] = []
+        self.sequenceNumber: int = 0
 
     def discover(self) -> list[dict[str, str]]:
         def receive():
@@ -38,7 +38,7 @@ class MilightController:
                 data, _ = discoverer.recvfrom(1024)
                 self.__handle_message(data)
 
-        discoverer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        discoverer: socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         discoverer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         discoverer.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         discoverer.bind(("0.0.0.0", self.port))
@@ -46,7 +46,7 @@ class MilightController:
         threading.Thread(target=receive, daemon=True).start()
 
         try:
-            discoverer_attempts = 10
+            discoverer_attempts: int = 10
             print(f"Sending discover request {discoverer_attempts} times")
             for _ in range(discoverer_attempts):
                 if self.discover_legacy:
@@ -68,27 +68,9 @@ class MilightController:
         else:
             return None
 
-    def __handle_message(self, message: bytes) -> None:
-        data = message.decode("ascii").split(",")
-        if len(data) >= 2:
-            ip = data[0]
-            mac = "".join([f"{x:02X}:" for x in bytearray(data[1], "ascii")])[:-1]
-            name = data[2]
-            type = "legacy" if name == "" else "v6"
-            port = self.__PORT_v4 if name == "" else self.__PORT_v6
-            new_device = {
-                "ip": ip,
-                "port": port,
-                "mac": mac,
-                "name": name,
-                "type": type,
-            }
-            if new_device not in self.disco_results:
-                self.disco_results.append(new_device)
-
     def establish_session(self, udp_socket: socket, device: dict) -> tuple[str, str]:
         # Send command to get Wifi Bridge Session ID
-        command = bytes.fromhex(
+        command: bytes = bytes.fromhex(
             "20 00 00 00 16 02 62 3A D5 ED A3 01 AE 08 2D 46 61 41 A7 F6 DC AF D3 E6 00 00 1E"
         )
         udp_socket.sendto(command, (device.get("ip"), device.get("port")))
@@ -97,12 +79,12 @@ class MilightController:
         # Receive responses
         while True:
             data, _ = udp_socket.recvfrom(1024)
-            response = data.hex()
+            response: str = data.hex()
             print("Got response: ", self.__add_spaces_to_hex(response))
 
             # Extract WB1 and WB2 from the response
-            wb1 = response[38:40]
-            wb2 = response[40:42]
+            wb1: str = response[38:40]
+            wb2: str = response[40:42]
             print("WB1:", wb1)
             print("WB2:", wb2)
 
@@ -116,29 +98,29 @@ class MilightController:
     # UDP Hex Send Format: 80 00 00 00 11 {WifiBridgeSessionID1} {WifiBridgeSessionID2} 00 {SequenceNumber} 00 {COMMAND} {ZONE NUMBER} 00 {Checksum}
     def send_command(self, device: dict, command: str, zone: Zone = Zone.ALL) -> str:
         # Create UDP socket
-        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_socket: socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         # Establish connection and get session ID
         wb1, wb2 = self.establish_session(udp_socket, device)
 
         # Calculate sequence number
         self.sequenceNumber = (self.sequenceNumber + 1) % 255
-        sn = "%02X" % self.sequenceNumber
+        sn: str = "%02X" % self.sequenceNumber
         
         # Get zone code (default "00")
-        zone_hex = zone.value
+        zone_hex: str = zone.value
 
         # Generate packet
-        packet = "80 00 00 00 11 {} {} 00 {} 00 {} {} 00".format(wb1, wb2, sn, command, zone_hex)
+        packet: str = "80 00 00 00 11 {} {} 00 {} 00 {} {} 00".format(wb1, wb2, sn, command, zone_hex)
 
         # Calculate packet checksum
-        checksum = self.__calc_checksum(packet.strip(" "))
+        checksum: str = self.__calc_checksum(packet.strip(" "))
 
         # Add checksum to the end of the packet
         packet += checksum
 
         # Convert string to bytes
-        command = bytes.fromhex(packet)
+        command: bytes = bytes.fromhex(packet)
 
         # Send the command
         udp_socket.sendto(command, (device.get("ip"), device.get("port")))
@@ -146,7 +128,7 @@ class MilightController:
 
         # Receive response
         response, _ = udp_socket.recvfrom(1024)
-        response_hex = response.hex()
+        response_hex: str = response.hex()
         print("Received response:", self.__add_spaces_to_hex(response_hex))
 
         # Close the socket
@@ -154,23 +136,40 @@ class MilightController:
 
         return response_hex
 
+    def __handle_message(self, message: bytes) -> None:
+        data = message.decode("ascii").split(",")
+        if len(data) >= 2:
+            ip: str = data[0]
+            mac: str = "".join([f"{x:02X}:" for x in bytearray(data[1], "ascii")])[:-1]
+            name: str = data[2]
+            type: str = "legacy" if name == "" else "v6"
+            port: str = self.__PORT_v4 if name == "" else self.__PORT_v6
+            new_device: dict[str, str] = {
+                "ip": ip,
+                "port": port,
+                "mac": mac,
+                "name": name,
+                "type": type,
+            }
+            if new_device not in self.disco_results:
+                self.disco_results.append(new_device)
+
     def __calc_checksum(self, hex_string: str) -> str:
-        if (
-            len(hex_string) >= 22
-        ):  # Each hex value is 2 characters, so 11 values means 22 characters
+        if len(hex_string) >= 22:
+            # Each hex value is 2 characters, so 11 values means 22 characters
             # Convert hex string to bytes
-            bytes_array = bytes.fromhex(hex_string)
+            bytes_array: bytes = bytes.fromhex(hex_string)
 
             # Calculate the modulo 256 checksum
-            checksum = sum(bytes_array[-11:]) % 256
+            checksum: int = sum(bytes_array[-11:]) % 256
 
             # Append the checksum to the bytes array
             bytes_array += bytes([checksum])
 
             # Convert bytes back to hex string
-            hex_string_with_checksum = bytes_array.hex()
+            hex_string_with_checksum: str = bytes_array.hex()
         else:
-            hex_string_with_checksum = hex_string
+            hex_string_with_checksum: str = hex_string
 
         return hex_string_with_checksum[-2:]
 
