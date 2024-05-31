3b02d2d996bad1130ebe143c3da258e7a6045fb6
wyz3r0 <dobroszek.d@gmail.com>
2024-06-01 01:17:57 +0200

    new version

--- MilightController/__init__.py
+++ MilightController/__init__.py
@@ -2,5 +2,5 @@ from .MilightController import MilightController
 from .Commands import Commands
 from .Zone import Zone
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __name__ = "MilightController"
